e0e53a081acbda31f86a0e6cac154799bc139b93
tunsay <tuna1997@hotmail.fr>
2023-07-27 16:35:31 +0200

    add documentation jsdoc

--- datepicker/src/App.js
+++ datepicker/src/App.js
@@ -2,15 +2,34 @@ import React, { useState } from 'react'
 
 import { DateLayoutPicker } from 'tunsay-datepicker-package'
 
+/**
+ * Root component of the application.
+ * This component uses the DateLayoutPicker component from the tunsay-datepicker-package
+ * to display a form with a Date of Birth field.
+ */
 const App = () => {
+  // Local state to store form data
   const [formData, setFormData] = useState({
     dateofbirth: null
   })
 
+  /**
+   * Handles the form submission.
+   * This function is called when the user submits the form.
+   * It prevents the default form behavior (page reload).
+   * @param {Event} e - The form submission event.
+   */
   const handleSubmit = (e) => {
     e.preventDefault()
+    // You can add the logic to process the form data here.
   }
 
+  /**
+   * Handles the change of selected date of birth.
+   * This function is called when the user selects a new date in the DateLayoutPicker.
+   * It updates the local state formData with the newly selected date.
+   * @param {Date} date - The date of birth selected by the user.
+   */
   const handleDateChange = (date) => {
     setFormData({ ...formData, dateofbirth: date })
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
 
+/**
+ * DateLayoutPicker Component
+ * A custom date picker component that wraps the React DatePicker component.
+ * This component simplifies the usage of the React DatePicker by providing a predefined layout.
+ * It renders an input field with a calendar picker for selecting dates.
+ *
+ * @component
+ * @param {Object} props - The props object containing the following properties:
+ * @param {string} props.name - The name of the input field.
+ * @param {Date} props.selected - The currently selected date.
+ * @param {function} props.onChange - A callback function to handle date changes.
+ * @returns {JSX.Element} - The JSX element representing the DateLayoutPicker component.
+ *
+ */
 export const DateLayoutPicker = ({ name, selected, onChange }) => {
   return (
     <DatePicker
